f4d6cbc2036436937a98f568cec33244fc9564af
MURALITHARAN D <dmmuralitharan@gmail.com>
2024-02-03 21:35:06 +0530

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarViewComponent } from './components/calendar-view/calendar-view.component';
 
-const routes: Routes = [
-  { path: '', component: CalendarViewComponent },
-];
+const routes: Routes = [];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
