1d1f05f63bc0795026a6acb1f584a36a4b3a49a7
MURALITHARAN D <dmmuralitharan@gmail.com>
2024-02-04 09:23:43 +0530

    merge two component to one component( calendar-view )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { CalendarViewComponent } from './components/calendar-view/calendar-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     CalendarViewComponent
   ],
   imports: [
--- src/app/components/calendar-view/calendar-view.component.ts
+++ src/app/components/calendar-view/calendar-view.component.ts
@@ -7,12 +7,17 @@ import { CalendarService } from '../../services/calendar.service';
   styleUrl: './calendar-view.component.css',
 })
 export class CalendarViewComponent {
+  monthYearTitle: string = '';
+  
   days: string[] = []
   currentMonthDates: number[] = [];
   previousMonthDates: number[] = [];
   nextMonthDates: number[] = [];
 
   constructor(private _calendar: CalendarService) {
+    this._calendar.changeCurrentMonth();
+    this.monthYearTitle = this._calendar.monthYearTitle;
+    
     this.days = this._calendar.days;
     this._calendar.previousMonthDates$.subscribe((previousMonthDate) => {
       this.previousMonthDates = previousMonthDate;
@@ -23,5 +28,20 @@ export class CalendarViewComponent {
     this._calendar.nextMonthDates$.subscribe((nextMonthDate) => {
       this.nextMonthDates = nextMonthDate;
     });
+  }
+
+  currentMonthBtn(): void {
+    this._calendar.changeCurrentMonth();
+    this.monthYearTitle = this._calendar.monthYearTitle;
+  }
+
+  previousMonthBtn(): void {
+    this._calendar.changePreviousMonth();
+    this.monthYearTitle = this._calendar.monthYearTitle;
+  }
+
+  nextMonthBtn(): void {
+    this._calendar.changeNextMonth();
+    this.monthYearTitle = this._calendar.monthYearTitle;
   }
 }
